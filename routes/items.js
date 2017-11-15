64e914124c67e8f0fc20aa11628e0740d1d45312
Jordan Moss <jmoss636@gmail.com>
2017-11-15 03:50:21 +0000

    Added lodash, added top story route

--- routes/items.js
+++ routes/items.js
@@ -1,30 +1,45 @@
-var express = require('express')
-var router = express.Router()
-var axios = require('axios')
+const express = require('express')
+const router = express.Router()
+const axios = require('axios')
+const _ = require('lodash')
+const URI = 'https://hacker-news.firebaseio.com/v0/item/'
+let topStoryIDs = getTopStoryIDs()
 
-/* GET dummy data. */
-router.get('/dummyData', function(req, res, next) {
-  //res.send('respond with a resource');
-  res.send({
-        "data": [
-            {
-                "desc": "In-Browser Find and Replace with RegEx Previews",
-                "date": "1510535202",
-                "author": "dalimil_",
-                "title": "google.com",
-                "votes": "1",
-                "comments": "0",
-                "link": "https://chrome.google.com/webstore/detail/find-replace-for-text-edi/jajhdmnpiocpbpnlpejbgmpijgmoknnl",
-                "id": "15683525",
-                "tags": "library,web,development"
-            }
-        ]  
-  })
-})
+async function getTopStoryIDs() {
+    let IDs = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json')
+    topStoryIDs = await IDs.data
+}
 
-router.get('/test', (req, res, next) => {
-  axios.get('https://hacker-news.firebaseio.com/v0/item/192327.json')
+async function getItemWithID(itemID){
+    const response = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${itemID}.json`)
+    return response.data
+}
+
+router.get('/test', (req, res) => {
+  axios.get('https://hacker-news.firebaseio.com/v0/item/192327.json?print=pretty')
         .then(result => res.send(result.data))
+        .catch(err => {throw err})
 })
 
+router.get('/topstories/:page', (req, res) => {
+    const page = Number(req.params.page)
+    const lowerBracket = page * 12
+    const upperBracket = (page + 1) * 12
+    const stories = []
+    
+    const requestedStoriesIDs = _.slice(topStoryIDs, lowerBracket, upperBracket)
+    requestedStoriesIDs.forEach(async (element) => {
+        stories.push(await getItemWithID(element))
+        if(_.compact(stories).length >= 12){
+            return res.send(stories)
+        }
+    })
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
+        .then(result => res.send(result.data))
+        .catch(err => {throw err})
+})
 module.exports = router;
